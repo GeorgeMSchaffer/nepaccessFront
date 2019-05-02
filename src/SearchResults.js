1e2969bf01a8ee1b72093d3fb49351b461abaf06
josh <josh@whew>
2019-05-02 14:27:51 -0700

    Broken

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,35 +1,70 @@
 import React from 'react';
+import 'react-tabulator/lib/styles.css';
+import { ReactTabulator } from 'react-tabulator';
 
 class SearchResults extends React.Component {
 
 	// TODO: At some point, the database should probably be giving us the headers to use.
 
 	render() {
-		console.log("SearchResults");
-		const results = this.props.results;
+        console.log("SearchResults");
+        
+        
+        const columns = [
+            { title: "Title", field: "title", width: 150 },
+            { title: "Agency", field: "agency" },
+            { title: "Comment date", field: "commentDate" },
+            { title: "Register date", field: "registerDate" },
+            { title: "State", field: "state" },
+            { title: "Version", field: "documentType" }
+        ];
+
+        var data = [
+            { title: "1", agency: "2", commentDate: "3", registerDate: "4", state: "5", documentType: "6"}
+        ];
+
+        const results = this.props.results;
+
+        // var data = results.map((result, idx) =>{
+        //     var newObject = {title: result.title, agency: result.agency, commentDate: result.commentDate, 
+        //     registerDate: result.registerDate, state: result.state, documentType: result.documentType};
+        //     return newObject;
+        // });
+
+        console.log(results);
+        console.log(data);
+
+		// const results = this.props.results;
 		return (
-			<table>
-				<thead><tr>
-				<th>Title</th>
-				<th>Agency</th>
-				<th>Comments date</th>
-				<th>Register date</th>
-				<th>State</th>
-				<th>Document type</th>
-				</tr></thead>
-				<tbody>
-					{results.map((result, idx) =>
-						<tr key={idx}>
-							<td><a href={result.documents} className='detailLink' id={`eis${result.id}`}>{result.title}</a></td>
-							<td>{result.agency}</td>
-							<td>{result.commentDate}</td>
-							<td>{result.registerDate}</td>
-							<td>{result.state}</td>
-							<td>{result.documentType}</td>
-						</tr>
-					)}
-				</tbody>
-			</table>
+			// <table>
+			// 	<thead><tr>
+			// 	<th>Title</th>
+			// 	<th>Agency</th>
+			// 	<th>Comments date</th>
+			// 	<th>Register date</th>
+			// 	<th>State</th>
+			// 	<th>Version</th>
+			// 	</tr></thead>
+			// 	<tbody>
+			// 		{results.map((result, idx) =>
+			// 			<tr key={idx}>
+			// 				{/* <td><a href={result.documents} className='detailLink' id={`eis${result.id}`}>{result.title}</a></td> */}
+			// 				<td>{result.title}</td>
+            //                 <td>{result.agency}</td>
+			// 				<td>{result.commentDate}</td>
+			// 				<td>{result.registerDate}</td>
+			// 				<td>{result.state}</td>
+			// 				<td>{result.documentType}</td>
+			// 			</tr>
+			// 		)}
+			// 	</tbody>
+            // </table>
+              <ReactTabulator
+                data={data}
+                columns={columns}
+                tooltips={true}
+                layout={"fitData"}
+                />
 		)
 	}
 }
--- src/Searcher.js
+++ src/Searcher.js
@@ -26,7 +26,6 @@ class Searcher extends React.Component {
 	onKeyUp = (evt) => {
 		// get the evt.target.name (defined by name= in input)
 		// and use it to target the key on our `state` object with the same name, using bracket syntax
-		// also need to wrap it in searcherInputs, or rename everything with prepending "searcherInputs."
 		this.setState( 
 		{ 
 			[evt.target.name]: evt.target.value
@@ -34,6 +33,15 @@ class Searcher extends React.Component {
 			this.debouncedSearch(this.state);
 			// console.log("Searcher state");
 			// console.log(this.state);
+		});
+    }
+    
+	onChecked = (evt) => {
+	    this.setState( 
+		{ 
+			[evt.target.name]: evt.target.checked
+		}, () => { // callback ensures state is set before state is used for search
+			this.debouncedSearch(this.state);
 		});
 	}
 	
@@ -62,6 +70,12 @@ class Searcher extends React.Component {
 					<option value="AK">Alaska</option><option value="AL">Alabama</option><option value="AQ">Antarctica</option><option value="AR">Arkansas</option><option value="AS">American Samoa</option><option value="AZ">Arizona</option><option value="CA">California</option><option value="CO">Colorado</option><option value="CT">Connecticut</option><option value="DC">District of Columbia</option><option value="DE">Delaware</option><option value="FL">Florida</option><option value="GA">Georgia</option><option value="GU">Guam</option><option value="HI">Hawaii</option><option value="IA">Iowa</option><option value="ID">Idaho</option><option value="IL">Illinois</option><option value="IN">Indiana</option><option value="KS">Kansas</option><option value="KY">Kentucky</option><option value="LA">Louisiana</option><option value="MA">Massachusetts</option><option value="MD">Maryland</option><option value="ME">Maine</option><option value="MI">Michigan</option><option value="MN">Minnesota</option><option value="MO">Missouri</option><option value="MS">Mississippi</option><option value="MT">Montana</option><option value="NAT">National</option><option value="NC">North Carolina</option><option value="ND">North Dakota</option><option value="NE">Nebraska</option><option value="NH">New Hampshire</option><option value="NJ">New Jersey</option><option value="NM">New Mexico</option><option value="NV">Nevada</option><option value="NY">New York</option><option value="OH">Ohio</option><option value="OK">Oklahoma</option><option value="OR">Oregon</option><option value="PA">Pennsylvania</option><option value="PR">Puerto Rico</option><option value="RI">Rhode Island</option><option value="SC">South Carolina</option><option value="SD">South Dakota</option><option value="TN">Tennessee</option><option value="TT">Trust Territory of the Pacific Islands</option><option value="TX">Texas</option><option value="UT">Utah</option><option value="VA">Virginia</option><option value="VI">Virgin Islands</option><option value="VT">Vermont</option><option value="WA">Washington</option><option value="WI">Wisconsin</option><option value="WV">West Virginia</option><option value="WY">Wyoming</option>
 				</select>
 				</div>
+                <div>
+                    <label>
+                        Must have comments
+                    </label>
+                    <input type="checkbox" name="needsComments" onChange={this.onChecked} />
+                </div>
 				<br />
 			</form>
         )
