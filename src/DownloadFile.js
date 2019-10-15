7b08aa3255111c348aa7f4d2619b631ddfceb64d
josh <josh@whew>
2019-10-15 10:01:05 -0700

    Update DownloadFile.js

--- src/DownloadFile.js
+++ src/DownloadFile.js
@@ -2,9 +2,9 @@ import React from 'react';
 import axios from 'axios';
 import './login.css';
 import Globals from './globals.js';
-// TODO: Pass props to this from SearchResults
 class DownloadFile extends React.Component {
 
+  // Receives props from SearchResults
 	constructor(props){
 		super(props);
         this.state = { 
@@ -13,8 +13,9 @@ class DownloadFile extends React.Component {
             downloadClass: 'download'
         };
 	}
-
-    download = (_filename) =>{ // TODO: Create state for row based on filename, use to update download progress?
+    // TODO: Cell resets to default state after saving/resizing window due to parent (SearchResults) re-rendering
+    // - is this behavior fine or do we want to preserve this state and for how long?
+    download = (_filename) =>{ 
         const FileDownload = require('js-file-download');
         
         // Progress percentage and indication it is downloading
@@ -41,37 +42,40 @@ class DownloadFile extends React.Component {
           
           // Indicate download completed as file is saved/prompted save as (depending on browser settings)
           this.setState({
-            downloadText: 'Downloaded'
+            downloadText: 'Done'
           });
           FileDownload(response.data, _filename);
           // verified = response && response.status === 200;
         })
         .catch((err) => { // TODO: This will catch a 404
           // console.log(err);
+          this.setState({
+            downloadText: 'Error: Not found'
+          });
         });
         
       }
 
     render(){
-        if(this.props){
-            if(this.props.downloadType === "Comments"){
-              const cellData = this.props.cell._cell.row.data;
-              if(cellData.commentsFilename){
-                  return <a className={this.state.downloadClass} onClick={() => {this.download(cellData.commentsFilename)} }>{this.state.downloadText} {this.state.progressValue}</a>;
-              } else { // Just return the blank unformatted string if that's what we have
-                  return cellData.commentsFilename;
-              }
-            } else if(this.props.downloadType === "EIS"){
-              const cellData = this.props.cell._cell.row.data;
-              if(cellData.filename){
-                  return <a className={this.state.downloadClass} onClick={() => {this.download(cellData.filename)} }>{this.state.downloadText} {this.state.progressValue}</a>;
-              } else { // Just return the blank unformatted string if that's what we have
-                  return cellData.filename;
-              }
-            }
-          } else {
-              return "";
+      if(this.props){
+        const cellData = this.props.cell._cell.row.data;
+        if(this.props.downloadType === "Comments"){
+          if(cellData.commentsFilename){
+              return <a className={this.state.downloadClass} onClick={() => {this.download(cellData.commentsFilename)} }>{this.state.downloadText} {this.state.progressValue}</a>;
+          } else { // Just return the blank unformatted string if that's what we have
+              return cellData.commentsFilename;
           }
+        } else if(this.props.downloadType === "EIS"){
+          if(cellData.filename){
+              console.log("Rendering download link for " + cellData.filename);
+              return <a className={this.state.downloadClass} onClick={() => {this.download(cellData.filename)} }>{this.state.downloadText} {this.state.progressValue}</a>;
+          } else { // Just return the blank unformatted string if that's what we have
+              return cellData.filename;
+          }
+        }
+      } else {
+          return "";
+      }
     }
 }
 
