0fd89809b0133bf429230769c7232d35946b9060
josh <josh@whew>
2020-02-27 15:24:42 -0700

    Modal: Pagination, scroll lock

--- src/MatchResults.js
+++ src/MatchResults.js
@@ -75,7 +75,7 @@ class MatchResults extends React.Component {
                 tooltips:true,
                 responsiveLayout:"collapse",  //collapse columns that dont fit on the table
                 pagination:"local",       //paginate the data
-                paginationSize:"10",       //allow 10 rows per page of data
+                paginationSize:10,       //allow 10 rows per page of data
                 paginationSizeSelector:[10, 25, 50, 100],
                 movableColumns:true,      //allow column order to be changed
                 resizableRows:true,       //allow row order to be changed
@@ -87,7 +87,7 @@ class MatchResults extends React.Component {
 
             // Could use just a bool prop to build the results text here instead of resultsText prop?
             return (
-                <div>
+                <div className='modal-results'>
                     {/* <h2>{results.length} Results</h2> */}
                     <h2>{this.props.resultsText}</h2>
                     <ReactTabulator
