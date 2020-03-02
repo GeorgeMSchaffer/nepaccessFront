c172b4909de52f1b2e31c87a3922ee1fc5493b67
josh <josh@whew>
2020-03-02 14:11:37 -0700

    Starting landing rework

--- src/Landing.js
+++ src/Landing.js
@@ -37,18 +37,20 @@ class Landing extends React.Component {
 
     render(){
       return (
-        <div id="landing">
+        <div>
           <div id="landing-images">
-              <div id="image-1"></div>
-              <div id="image-2"></div>
+            <div id="image-1">
+              <span id="headline" className="no-select cursor-default">
+                Headline goes here
+              </span>
+            </div>
+            <div id="image-2">
+              <span id="subhead" className="no-select cursor-default">
+                Subhead here
+              </span>
+            </div>
           </div>
 
-          <span id="headline" className="no-select cursor-default">
-            Headline goes here: Find NEPA documents and find em good
-          </span>
-          <span id="subhead" className="no-select cursor-default">
-            Subhead here. The viewer should understand concisely what the site does for them.
-          </span>
 
           <div id="search-container">
             <SearcherLanding 
