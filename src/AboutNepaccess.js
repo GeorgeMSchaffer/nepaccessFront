c78eb419ed772dc6284730b5b86b4fdd05ee0ad3
josh <josh@whew>
2020-05-13 16:12:44 -0700

    Text fixes

--- src/AboutNepaccess.js
+++ src/AboutNepaccess.js
@@ -20,13 +20,13 @@ class AboutNepaccess extends React.Component {
                         NEPAccess helps bring <Link className="about-nepa-button" to="/aboutnepa">NEPA</Link> into the 21st century by harnessing the power of data science to enable the full vision of NEPA as a strategy to deal with emerging 21st-century social, economic, and environmental challenges.
                     </span>
                     <span className="default-style">
-                        NEPAccess is housed at the University of Arizona and is supported by <a id="link-award" href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=1831551">a research grant from the National Science Foundation (no. 1831551, Dr. Laura López-Hoffman, PI)</a>, with additional resources from the University of Arizona and the Morris K. Udall and Stewart L. Udall 
+                        NEPAccess is housed at the University of Arizona and is supported by <a id="link-award" href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=1831551">a research grant from the National Science Foundation (no. 1831551, Dr. Laura López-Hoffman, PI)</a>, with additional resources from the University of Arizona and the Morris K. Udall and Stewart L. Udall Foundation.
                     </span>
                     <span className="default-style">
-                        The NEPAccess team is interdisciplinary (principally based at UArizona, with one member from the University of California, Davis) and comprises researchers from data science, natural language processing (NLP), public administration, political science, law, natural resources policy, and UX design and 
+                        The NEPAccess team is interdisciplinary (principally based at UArizona, with one member from the University of California, Davis) and comprises researchers from data science, natural language processing (NLP), public administration, political science, law, natural resources policy, and UX design and outreach.
                     </span>
                     <span className="default-style">
-                        NEPAccess is a knowledge catalyst and decision-making platform that provides access to thousands of NEPA documents and makes available for the first-time analytical tools that enable systematic research by scholars in the social and natural sciences, law, and public health, and application by NEPA practitioners, project proponents, and a range of 
+                        NEPAccess is a knowledge catalyst and decision-making platform that provides access to thousands of NEPA documents and makes available for the first-time analytical tools that enable systematic research by scholars in the social and natural sciences, law, and public health, and application by NEPA practitioners, project proponents, and a range of stakeholders.
                     </span>
                     <span className="default-style">
                         Using cutting-edge NLP and other analytic tools, scholars, citizens, environmental professionals, and agency staff can use NEPAccess to answer a host of critical questions about how humans impact the environment and how the environment impacts us.
