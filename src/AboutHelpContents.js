f072f7cd82708068e949f58de1731a0ca8fb60ea
josh <josh@whew>
2020-12-11 11:47:38 -0700

    Added new dynamic counts, new wording

--- src/AboutHelpContents.js
+++ src/AboutHelpContents.js
@@ -1,8 +1,131 @@
 import React from 'react';
+import axios from 'axios';
+
+import Globals from './globals.js';
+
 import './aboutNepa.css';
 import './aboutHelp.css';
 
 export default class AboutHelpContents extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            finalCount: null,
+            draftCount: null,
+            finalCountDownloadable: null,
+            draftCountDownloadable: null
+        }
+    }
+    
+    getFinalCount = () => {
+        let getUrl = Globals.currentHost + "stats/final_count";
+        
+        axios.get(getUrl, {
+            params: {
+                
+            }
+        }).then(response => {
+            let responseOK = response && response.status === 200;
+            if (responseOK && response.data) {
+                return response.data;
+            } else {
+                return null;
+            }
+        }).then(parsedJson => { 
+            console.log(parsedJson);
+            if(parsedJson){
+                this.setState({
+                    finalCount: parsedJson
+                });
+            } else { // null/404
+
+            }
+        }).catch(error => {
+            
+        });
+    }
+    getFinalCountDownloadable = () => {
+        let getUrl = Globals.currentHost + "stats/final_count_downloadable";
+        
+        axios.get(getUrl, {
+            params: {
+                
+            }
+        }).then(response => {
+            let responseOK = response && response.status === 200;
+            if (responseOK && response.data) {
+                return response.data;
+            } else {
+                return null;
+            }
+        }).then(parsedJson => { 
+            console.log(parsedJson);
+            if(parsedJson){
+                this.setState({
+                    finalCountDownloadable: parsedJson
+                });
+            } else { // null/404
+
+            }
+        }).catch(error => {
+            
+        });
+    }
+    getDraftCount = () => {
+        let getUrl = Globals.currentHost + "stats/draft_count";
+        
+        axios.get(getUrl, {
+            params: {
+                
+            }
+        }).then(response => {
+            let responseOK = response && response.status === 200;
+            if (responseOK && response.data) {
+                return response.data;
+            } else {
+                return null;
+            }
+        }).then(parsedJson => { 
+            console.log(parsedJson);
+            if(parsedJson){
+                this.setState({
+                    draftCount: parsedJson
+                });
+            } else { // null/404
+
+            }
+        }).catch(error => {
+            
+        });
+    }
+    getDraftCountDownloadable = () => {
+        let getUrl = Globals.currentHost + "stats/draft_count_downloadable";
+        
+        axios.get(getUrl, {
+            params: {
+                
+            }
+        }).then(response => {
+            let responseOK = response && response.status === 200;
+            if (responseOK && response.data) {
+                return response.data;
+            } else {
+                return null;
+            }
+        }).then(parsedJson => { 
+            console.log(parsedJson);
+            if(parsedJson){
+                this.setState({
+                    draftCountDownloadable: parsedJson
+                });
+            } else { // null/404
+
+            }
+        }).catch(error => {
+            
+        });
+    }
     
     render () {
         return (
@@ -24,16 +147,18 @@ export default class AboutHelpContents extends React.Component {
                     </div>
                     
                     <div><p>
-                        This includes draft and final documents. NEPA access is a work in progress—as time goes on, other documents related to the National Environmental Policy Act of 1969 (NEPA) will be added.
+                        This includes {this.state.draftCount} draft and {this.state.finalCount} final documents. 
+                        Of these, {this.state.draftCountDownloadable} draft and {this.state.finalCountDownloadable} final EISs are in a format that supports full-text searching and downloading.
+                        </p><p>NEPAccess is a work in progress—as time goes on, other documents related to the National Environmental Policy Act of 1969 (NEPA) will be added.
                     </p></div>
 
-                    <h2>
+                    {/* <h2>
                         Downloadable Files
                     </h2>
 
                     <div>
                         There are currently 1513 downloadable EIS archives available. Because EIS’s are often split into multiple files, the downloaded archive may contain more than one PDF.
-                    </div>
+                    </div> */}
 
                     <div>
                         Efforts to compile a complete set of documents are ongoing.
@@ -42,5 +167,12 @@ export default class AboutHelpContents extends React.Component {
                 </div>
             </div>
         );
+    }
+
+    componentDidMount() {
+        this.getDraftCount();
+        this.getFinalCount();
+        this.getDraftCountDownloadable();
+        this.getFinalCountDownloadable();
     }
 }
