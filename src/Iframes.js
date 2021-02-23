4f3827bd6b1217210dbe4afbd8b027b93d321a9f
josh <josh@whew>
2021-02-23 13:20:45 -0700

    New iframe test page

---
+++ src/Iframes.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './iframes.css';
+
+export default class Iframes extends React.Component {
+    render() {
+        return (
+            <div className="iframe-container">
+                {/* style={{overflow: 'hidden', marginTop: '0px'}}>* /}
+                {/* <iframe src="https://techpatterns.com/web_design/frames_iframes/iframes.php" */}
+                <iframe src="http://localhost:3000/aboutnepaccess"
+                    scrolling="yes" frameborder="0" width="100%" height="100%" name="ContentCenter">
+                        <p>
+                            This should only display if your browser doesn't support iframes 
+                            or if you have iframe support turned off.
+                        </p>
+                    </iframe>
+            </div>
+        );
+    }
+}
