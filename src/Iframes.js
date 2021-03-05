1330cdbe03151ef0924438c14560d60c047479f3
josh <josh@whew>
2021-03-05 10:13:41 -0700

    Update Iframes.js

--- src/Iframes.js
+++ src/Iframes.js
@@ -7,7 +7,7 @@ export default class Iframes extends React.Component {
             <div className="iframe-container">
                 {/* style={{overflow: 'hidden', marginTop: '0px'}}>* /}
                 {/* <iframe src="https://techpatterns.com/web_design/frames_iframes/iframes.php" */}
-                <iframe src="https://about.nepaccess.org/about-nepa/"
+                <iframe src="https://about.nepaccess.org/about-nepa/" title="Title"
                     scrolling="yes" frameborder="0" width="100%" height="100%" name="ContentCenter">
                         <p>
                             This should only display if your browser doesn't support iframes 
