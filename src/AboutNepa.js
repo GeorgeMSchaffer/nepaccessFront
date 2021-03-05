122aeb4c82d5b4fc7a5b013fc1fae42ee3f11316
josh <josh@whew>
2021-03-04 17:44:31 -0700

    Update AboutNepa.js

--- src/AboutNepa.js
+++ src/AboutNepa.js
@@ -7,7 +7,7 @@ export default class AboutNepa extends React.Component {
             <div className="iframe-container">
                 {/* style={{overflow: 'hidden', marginTop: '0px'}}>* /}
                 {/* <iframe src="https://techpatterns.com/web_design/frames_iframes/iframes.php" */}
-                <iframe src="https://about.nepaccess.org/about-nepa/"
+                <iframe src="https://about.nepaccess.org/about-nepa/" title="About Nepa"
                     scrolling="yes" frameborder="0" width="100%" height="100%" name="ContentCenter">
                         <p>
                             This should only display if your browser doesn't support iframes 
