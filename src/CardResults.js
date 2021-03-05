dc3c5b0a1a893843f5db76ba2ea4976bedbd5395
josh <josh@whew>
2021-03-05 12:51:24 -0700

    Update CardResults.js

--- src/CardResults.js
+++ src/CardResults.js
@@ -54,12 +54,14 @@ class CardResults extends React.Component {
         console.log("searching",nextProps.searching===this.props.searching); 
         console.log("snippets",nextProps.snippetsDisabled===this.props.snippetsDisabled); 
 
-        // TODO: Experimental logic, we shouldn't need to rerender JUST because searching changed
-        // if(nextProps.results===this.props.results
-        //         && nextProps.resultsText===this.props.resultsText
-        //         && nextProps.snippetsDisabled===this.props.snippetsDisabled) {
-        //     return false;
-        // }
+        // TODO: Experimental logic, we shouldn't rerender JUST because results text changed
+        if(
+            nextProps.results===this.props.results
+            && !nextProps.resultsText===this.props.resultsText 
+            && nextProps.searching===this.props.searching 
+            && nextProps.snippetsDisabled===this.props.snippetsDisabled ) {
+                return false;
+        }
         return true;
     }
     
