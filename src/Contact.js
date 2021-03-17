302a011828cf73fbe4420a46e831d540e6936542
josh <josh@whew>
2021-03-17 14:55:33 -0700

    Update Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Select from 'react-select';
+// import Select from 'react-select';
 import Creatable from 'react-select/lib/Creatable';
 
 import axios from 'axios';
