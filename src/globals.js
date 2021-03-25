9e23192683a6257c87864560d2c7ee002340c8e8
josh <josh@whew>
2021-03-25 15:58:34 -0700

    Update globals.js

--- src/globals.js
+++ src/globals.js
@@ -32,7 +32,6 @@ const Globals = {
     // Set up globals like axios default headers and base URL
     // TODO: SSL: set to https here and elsewhere
     setUp() {
-        console.log(window.location.protocol);
         if(window.location.hostname === 'mis-jvinalappl1.microagelab.arizona.edu' || window.location.hostname === 'www.nepaccess.org') {
             this.currentHost = new URL(window.location.protocol + 'mis-jvinalappl1.microagelab.arizona.edu:8080/');
         } else {
