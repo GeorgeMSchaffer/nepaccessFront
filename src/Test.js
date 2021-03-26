d6496fa5e2285689c122552149c7ace3679b82e4
josh <josh@whew>
2021-03-26 11:16:16 -0700

    Testing recaptcha

--- src/Test.js
+++ src/Test.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import {Helmet} from "react-helmet";
+import {ReCAPTCHA} from "react-google-recaptcha";
 
 // import axios from 'axios';
 
 // import Globals from './globals.js';
+
+const recaptchaRef = React.createRef();
 
 export default class Test extends React.Component {
 
@@ -14,13 +17,21 @@ export default class Test extends React.Component {
         };
     }
 
-    onInput = (evt) => {
-        this.setState({ [evt.target.name]: evt.target.value });
+    onSubmit = () => {
+        const recaptchaValue = recaptchaRef.current.getValue();
+        console.log(recaptchaValue);
+        // this.props.onSubmit(recaptchaValue);
+    }
+    // onInput = (evt) => {
+    //     this.setState({ [evt.target.name]: evt.target.value });
+    // }
+    captchaChange(value) {
+        console.log("Captcha value:", value);
     }
 
     render () {
         return (
-            <div id="test-container">
+            <div id="test-container" className="content">
                 <Helmet>
                     <meta charSet="utf-8" />
                     <title>NEPAccess - Test</title>
@@ -28,6 +39,14 @@ export default class Test extends React.Component {
                 </Helmet>
                 
                 <span>test</span>
+                <form onSubmit={this.onSubmit}>
+                    <input type="button" onClick={this.onSubmit}/>
+                    <ReCAPTCHA
+                        ref={recaptchaRef}
+                        sitekey="6LdLG5AaAAAAADg1ve-icSHsCLdw2oXYPidSiJWq"
+                        onChange={this.captchaChange}
+                    />
+                </form>
             </div>
         )
     }
