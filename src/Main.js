2779a1929264569877df997efd5c57aaeb35a7d6
josh <josh@whew>
2021-04-02 12:12:39 -0700

    Tentative footer added in

--- src/Main.js
+++ src/Main.js
@@ -270,7 +270,7 @@ class Main extends React.Component {
 
                 <Route path="/" component={Landing}/>
             </Switch>
-            {/* <Footer id="footer"></Footer> */}
+            <Footer id="footer"></Footer>
         </div>
         )
     }
