eb63a3a47c8a817d9fa8397add4346a9ec9dd2b8
josh <josh@whew>
2021-04-13 12:21:20 -0700

    Update Iframes.js

--- src/iframes/Iframes.js
+++ src/iframes/Iframes.js
@@ -8,38 +8,18 @@ export default class Iframes extends React.Component {
 
     render(){
         return (
-            <>
-                {/* <IframeResizer
-                    title="Title"
-                    forwardRef={this.iframeRef}
-                    // ref="iframe" 
-                    heightCalculationMethod="lowestElement"
-                    inPageLinks
-                    log
-                    // onMessage={onMessage}
-                    // onResized={onResized}
-                    // onLoad={this.resizeIframe(this)}
-                    id="iframe-landing-container"
-                    src="https://about.nepaccess.org/about-nepa/"
-                    scrolling="auto"
-                    // scrolling="no" frameBorder="0" width="100%" height="100%" name="ContentCenter"
-                    style={{ width: '1px', minWidth: '100%', minHeight: '100%'}}
-                /> */}
-                <App />
-                {/* <IframeResizer
-                    log
-                    id="footer-2"
-                    src="https://about.nepaccess.org/footer/"
-                    style={{ width: '1px', minWidth: '100%'}}
-                /> */}
-                <iframe src="https://about.nepaccess.org/footer/" title="Footer" id="footer-2"
-                    scrolling="no" frameBorder="0" width="100%" height="100%" name="ContentCenter">
-                        <p>
-                            This should only display if your browser doesn't support iframes 
-                            or if you have iframe support turned off.
-                        </p>
-                </iframe>
-            </>
+            <div className="content">
+                <IframeResizer 
+                    src="https://about.nepaccess.org/about-nepa" 
+                    title="Footer"
+                    style={{ width: '1px', minWidth: '100%'}} 
+                />
+                <IframeResizer 
+                    src="https://about.nepaccess.org/elementor-hf/footer/" 
+                    title="Footer"
+                    style={{ width: '1px', minWidth: '100%'}} 
+                />
+            </div>
         )
         
       }
