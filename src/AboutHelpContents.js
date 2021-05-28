7e89026cd4c432d973708032d49042432e33ea44
josh <josh@whew>
2021-05-28 14:13:01 -0700

    final cleanup

--- src/AboutHelpContents.js
+++ src/AboutHelpContents.js
@@ -71,7 +71,6 @@ export default class AboutHelpContents extends React.Component {
                     <h2>
                         Environmental Impact Statements
                     </h2>
-                    
 
                     <div><p>
                         <span className="dynamic-stat">Blue</span> items are dynamic (retrieved on demand from database).
@@ -82,18 +81,10 @@ export default class AboutHelpContents extends React.Component {
                     </div>
                     
                     <div><p>
-                        This includes <span className="dynamic-stat">{this.state.draftCount}</span> draft and <span className="dynamic-stat">{this.state.finalCount}</span> final documents. 
-                        Of these, <span className="dynamic-stat">{this.state.draftCountDownloadable}</span> draft and <span className="dynamic-stat">{this.state.finalCountDownloadable}</span> final EISs are in a format that supports full-text searching and downloading.
+                        This includes <span className="dynamic-stat">{this.state.draftCount}</span> draft and <span className="dynamic-stat">{this.state.finalCount}</span> final EIS documents. 
+                        Of these, <span className="dynamic-stat">{this.state.draftCountDownloadable}</span> drafts and <span className="dynamic-stat">{this.state.finalCountDownloadable}</span> finals are in a format that supports full-text searching and downloading.
                         </p><p>NEPAccess is a work in progress—as time goes on, other documents related to the National Environmental Policy Act of 1969 (NEPA) will be added.  Total searchable texts (each one representing a .pdf): <span className="dynamic-stat">{this.state.textCount}</span>.
                     </p></div>
-
-                    {/* <h2>
-                        Downloadable Files
-                    </h2>
-
-                    <div>
-                        There are currently 1513 downloadable EIS archives available. Because EIS’s are often split into multiple files, the downloaded archive may contain more than one PDF.
-                    </div> */}
 
                     <div>
                         Efforts to compile a complete set of documents are ongoing.
