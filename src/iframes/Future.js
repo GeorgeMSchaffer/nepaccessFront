c0feabc022a01920117c9bfa613c11e14c0888f7
jvinal <deathpizza@hotmail.com>
2021-09-29 13:17:14 -0700

    Create Future.js

---
+++ src/iframes/Future.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Helmet} from 'react-helmet';
+import IframeResizer from 'iframe-resizer-react';
+
+export default class Future extends React.Component {
+    render() {
+        return (
+            <div className="iframe-container">
+                <Helmet>
+                    <meta charSet="utf-8" />
+                    <title>Future - NEPAccess</title>
+                    <link rel="canonical" href="https://nepaccess.org/future" />
+                </Helmet>
+
+                <IframeResizer
+                    // log
+                    data-hj-allow-iframe="true"
+                    id="iframe-landing-container"
+                    src="https://about.nepaccess.org/nepaccess-of-the-future/"
+                    style={{ width: '1px', minWidth: '100%'}}
+                />
+            </div>
+        );
+    }
+}
