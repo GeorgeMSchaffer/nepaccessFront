461b2517a098a9ea07a5005665319e2ccec151d7
jvinal <deathpizza@hotmail.com>
2021-10-12 15:50:00 -0700

    Update ForgotPassword.js

--- src/User/ForgotPassword.js
+++ src/User/ForgotPassword.js
@@ -90,7 +90,9 @@ class ForgotPassword extends React.Component {
         }).then(success => {
             if(success){
                 this.setState({
-                    successLabel: "Reset link sent to provided email address from Eller-NepAccess@email.arizona.edu. Please allow roughly one minute for email to arrive."
+                    successLabel: "Reset link sent to provided email address from Eller-NepAccess@email.arizona.edu."
+                    + " Please allow roughly one minute for email to arrive."
+                    + " If email doesn't arrive, please check your spam folder."
                 });
             } else {
                 // Server down?
