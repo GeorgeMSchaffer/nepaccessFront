cae9dd95976af75dc812243777f46367bb7d21c0
jvinal <deathpizza@hotmail.com>
2022-04-05 18:02:16 -0700

    Text

--- src/Details/ProcessDetailsTab.js
+++ src/Details/ProcessDetailsTab.js
@@ -194,7 +194,7 @@ export default class ProcessDetailsTab extends React.Component {
         return filenames.map((_filename, i) => {
             if(i === 0) {
                 if(!showMore) {
-                    return <span className="show-more-link" onClick={() => {this.showMoreToggle(_id)}}>Show individual file downloads ↴</span>
+                    return <span className="show-more-link" onClick={() => {this.showMoreToggle(_id)}}>Show individual file downloads</span>
                 } else {
                     return <>
                         <span className="show-more-link" onClick={() => {this.showMoreToggle(_id)}}>Hide individual file downloads</span>
