0a2fee7f7ccec94266415a1e48fc3ead30d03a46
jvinal <deathpizza@hotmail.com>
2022-07-08 13:05:08 -0700

    Need to refresh all results
    Prevents oddity where if new search terms get same records, old highlights are reused that don't reflect the new terms.

--- src/App.js
+++ src/App.js
@@ -467,6 +467,7 @@ export default class App extends React.Component {
 
 		this.setState({
             // Fresh search, fresh results
+            searchResults: [],
             outputResults: [],
             // geoResults: null,
             count: 0,
